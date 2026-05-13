ad1e54cdbb6db6394c163ab617be3f752c7e12fb
Tenjy-x <tendrymk848@gmail.com>
2026-05-13 12:58:59 +0300

    Controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'AuthController::form');
+$routes->get('/bo/dashboard/general', 'BOController::general');
+$routes->get('/rh/dashboard/general', 'RHController::general');
+$routes->get('/index' , 'UserController::index');
+
