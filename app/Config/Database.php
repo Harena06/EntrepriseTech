ef1cb873c39c197f4390fa17cf3d15dc03f50cfe
Harena06 <sahazaharenafeno@gmail.com>
2026-05-13 10:14:40 +0300

    model et base modif

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => '',
         'username'     => '',
         'password'     => '',
-        'database'     => WRITEPATH . 'Base/examen.db',
+        'database'     => WRITEPATH . 'examen.db',
         'DBDriver'     => 'SQLite3',
         'DBPrefix'     => '',
         'pConnect'     => false,
